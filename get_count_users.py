81d8cc5c84c481d3518c95debce45a0e9b8a48fc
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-26 02:19:37 +0500

    get_count_users

--- get_count_users.py
+++ get_count_users.py
@@ -12,12 +12,12 @@ def get_count_users(data:dict) -> int:
     """
     sum_users = 0
     results = data['results']
-    for user in range(len(results)):
-        if 'gender' in results[user]:
-            sum_users+=1
+    for user in results:
+        a = user['name']
+        sum_users+=1
 
         
-        return sum_users
+    return sum_users
 data = get_data('randomuser_data.json')
 print(get_count_users(data))
 
